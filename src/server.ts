5faf75431ea1b1f218d3d3e1a2457e2bfddf6ae8
itsmahmudul <islamashraf128@gmail.com>
2026-04-24 22:17:39 +0600

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
+import app from "./app";
+import config from "./app/config";
 
+const port = config.port || 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on post ${port}`);
+});
