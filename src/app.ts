114f568ebe72f6364b203273868b7c89f9b0dd73
itsmahmudul <islamashraf128@gmail.com>
2026-04-25 23:06:47 +0600

    added user auth route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { userRoutes } from "./app/modules/user/user.route";
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Vehicle Rental System API is running");
